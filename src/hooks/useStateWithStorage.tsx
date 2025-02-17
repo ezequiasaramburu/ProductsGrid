7749d177697bcff3a138a43a03a6cf4c979d4adf
Ezequias <ezequias.aramburu@gmail.com>
2025-02-17 15:50:56 +0100

    update stateWithStorage hook for persisting state in localStore

--- src/hooks/useStateWithStorage.tsx
+++ src/hooks/useStateWithStorage.tsx
@@ -1,4 +1,15 @@
-export default function useStateWithStorage(
-  key: string,
-  defaultValue: unknown
-) {}
+import { useState } from 'react';
+
+export default function useStateWithStorage<T>(key: string, defaultValue: T) {
+  const storedValue = localStorage.getItem(key);
+  const parsedValue = storedValue ? JSON.parse(storedValue) : null;
+
+  const [state, setState] = useState<T>(parsedValue || defaultValue);
+
+  const setStoredState = (newState: T) => {
+    setState(newState);
+    localStorage.setItem(key, JSON.stringify(newState));
+  };
+
+  return [state, setStoredState] as const;
+}
