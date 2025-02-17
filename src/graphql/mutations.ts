52d76bd18ffb1f0cee46d14c8955895a953fb65f
Ezequias <ezequias.aramburu@gmail.com>
2025-02-17 17:51:29 +0100

    update queries and mutations

--- src/graphql/mutations.ts
+++ src/graphql/mutations.ts
@@ -1,2 +1,13 @@
-// Here we put mutations. Remove next line
-export {};
+import { gql } from '@apollo/client';
+
+export const ADD_ITEM_TO_ORDER = gql`
+  mutation AddItemToOrder($productVariantId: ID!, $quantity: Int!) {
+    addItemToOrder(productVariantId: $productVariantId, quantity: $quantity) {
+      items {
+        id
+        productVariantId
+        quantity
+      }
+    }
+  }
+`;
--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -1,2 +1,19 @@
-// Here we put queries. Remove next line
-export {};
+import { gql } from '@apollo/client';
+
+export const GET_PRODUCTS = gql`
+  query {
+    products {
+      items {
+        id
+        name
+        description
+        assets {
+          source
+        }
+        variants {
+          price
+        }
+      }
+    }
+  }
+`;
